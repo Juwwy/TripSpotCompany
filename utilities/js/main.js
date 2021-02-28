a011a7e7e1b3f74e3ef8d281b9c4b4df726c0a2f
Olowu Juwon <juwonolowu52@gmail.com>
2021-02-28 19:21:38 +0100

    Trip spot travel project

---
+++ utilities/js/main.js
@@ -0,0 +1,8 @@
+var newMember;
+
+function myWelcomeMessage(){
+    newMember= prompt("Welcome! what is your name?");
+    alert("Wow! " + newMember + " " + "we are glad to have you on Trip Spot Travel official Page. Feel free to explore on our web page for better experience on your future tour campaign. Thank You!");
+}
+
+console.log(myWelcomeMessage());
